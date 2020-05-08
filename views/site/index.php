35aeaa517f4696caff0f1b426646cd2846141730
david tallafigo <david.florido@iesdonana.org>
2020-05-08 20:47:47 +0200

    Cambios en la pagina principal

--- views/site/index.php
+++ views/site/index.php
@@ -9,36 +9,44 @@ $titulo = Usuarios::findOne(Yii::$app->user->id);
 
 $this->title = 'Bienvenido ' . $titulo['log_us'];
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="site-index">
     <h1><?= Html::encode($this->title) ?></h1>
 
-        <?php $form = ActiveForm::begin(); ?>
-            <?= $form->field($publicar, 'text')->textarea(['maxlength' => true, 'placeholder' => 'Publica algo...', ]) ?>
-            <div class="form-group">        
-                <?= Html::submitButton('Publicar', ['class' => 'btn btn-success']) ?>
-            </div>
-        <?php ActiveForm::end(); ?>
+    <div class="row">
+        <div class="col">
+            <?php $form = ActiveForm::begin(); ?>
+                <?= $form->field($publicar, 'text')->textarea(['maxlength' => true, 'placeholder' => 'Publica algo...', ]) ?>
+                <div class="form-group">        
+                    <?= Html::submitButton('Publicar', ['class' => 'btn btn-success']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
 
-    <div class="container">
-        <div class="col-12 col-lg-6 mt-5 order-1 order-lg-0">
+    <div class="row">
+        <div class="col">
             <?php foreach ($comentarios as $comentario) : ?>
                 <?php 
                     $id = $comentario['usuario_id'];
                     $log = Usuarios::findOne(['id' => $id]);
+                    $url1 = Url::to(['comentarios/view', 'id' => $comentario['id']]);
                 ?>
-                <div class="row">
-                    <div class="col-md-4">
-                    <?php $form = ActiveForm::begin(); ?>
-                    <?php $url = Url::to(['usuarios/view', 'id' => $comentario['usuario_id']]); ?>
-                        <a href=<?= $url ?>><h3 class=""><?= $log['log_us']?></h3></a>
-                    <?php ActiveForm::end(); ?>
+                    <div class="card">
+                        <div class="card-header">
+                            <?= Html::a($log['log_us'], ['usuarios/view', 'id' => $id]) ?>
+                        </div>
+                        <a href="<?= $url1 ?>">
+                            <div class="card-body">
+                                <blockquote class="blockquote mb-0">
+                                    <p><?= $comentario['text'] ?></p>
+                                </blockquote>
+                            </div>
+                        </a>
                     </div>
-                    <div class="col-6 col-md-4 comentarios">
-                        <p class=""><?= $comentario['text']?></p>
-                    </div>
-                </div>
             <?php endforeach; ?>
+        </div>
     </div>
 
 
